9154944dc3aa506fdfeef884a281481a52a25e82
rafaScalet <rafaelscalet2018@gmail.com>
2024-05-06 18:31:52 -0300

    exercicio finalizado

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using consecionaria.models;
+
+Carro carro = new(200, "Ubuntu", "BYD");
+Moto moto = new(3000, "Roma", "IDE");
+
+Console.WriteLine("Carro - ");
+Console.WriteLine("Modelo: " + carro.Modelo);
+Console.WriteLine("Ano: " + carro.Ano);
+Console.WriteLine("\nMoto - ");
+Console.WriteLine("Modelo: " + moto.Modelo);
+Console.WriteLine("Ano: " + moto.Ano);
